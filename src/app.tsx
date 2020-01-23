4987cf78e6126cf6706704861f448e40063a82f4
Jega <jega@lirisoft.com>
2020-01-23 16:58:10 +0530

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import logo from './assets/logo.png'
+
+ReactDOM.render(<div>
+
+
+    <h3>  hello world from React!</h3>
+    <img src={logo}></img>
+
+
+</div>, document.getElementById('root'));
---
+++ webpack.rules.js
@@ -0,0 +1,40 @@
+module.exports = [
+  // Add support for native node modules
+  {
+    test: /\.node$/,
+    use: 'node-loader',
+  },
+  {
+    test: /\.(m?js|node)$/,
+    parser: { amd: false },
+    use: {
+      loader: '@marshallofsound/webpack-asset-relocator-loader',
+      options: {
+        outputAssetBase: 'native_modules',
+      },
+    },
+  },
+  {
+    test: /\.(ts|js)x?$/,
+    exclude: /(node_modules|.webpack)/,
+    loaders: [{
+      loader: 'ts-loader',
+      options: {
+        transpileOnly: true
+      }
+    }]
+  },
+
+  {
+    test: /\.(png|svg|jpg|gif)$/,
+    use: [
+      'file-loader'
+    ]
+  },
+  {
+    test: /\.(woff|woff2|eot|ttf|otf)$/,
+    use: [
+      'file-loader'
+    ]
+  }
+];
